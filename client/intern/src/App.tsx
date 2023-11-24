b88331c0508ce90868dbeb0aa35956f345847a27
Tarik Azzouzi <32739295+CodingTarik@users.noreply.github.com>
2023-11-24 16:37:35 +0100

    added some comments

--- client/intern/src/App.tsx
+++ client/intern/src/App.tsx
@@ -1,3 +1,4 @@
+// Importing necessary dependencies and styles
 import "./App.css";
 import ListItemDecorator from "@mui/joy/ListItemDecorator";
 import Tabs from "@mui/joy/Tabs";
@@ -11,23 +12,47 @@ import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import PolicyIcon from '@mui/icons-material/Policy';
 import SettingsIcon from '@mui/icons-material/Settings';
 
+/**
+ * Main component representing the entire application.
+ * @returns {JSX.Element} The rendered React element.
+ */
 function App() {
+  // State to manage the currently selected tab index
   const [index, setIndex] = React.useState(0);
+
+  // Color options for different tabs
   const colors = ["primary", "danger", "success", "warning"] as const;
+
+  // JSX structure representing the entire application
   return (
     <>
+      {/* 
+        Tabs component for navigation. 
+        - `size="lg"` sets the size of the tabs to large.
+        - `aria-label` provides an accessible label for screen readers.
+        - `value` represents the currently selected tab index.
+        - `onChange` is the callback function when a tab is changed.
+        - `sx` is a styling object defined using theme variables.
+      */}
       <Tabs
         size="lg"
         aria-label="Bottom Navigation"
         value={index}
-        onChange={(_eventf, value) => setIndex(value as number)}
+        onChange={(_event, value) => setIndex(value as number)}
         sx={(theme) => ({
           p: 1,
           borderRadius: 16,
           mx: "auto",
           mt: 0,
           boxShadow: theme.shadow.sm,
+          // Variables for customizing the shadow, is used in many componens of material ui
+          // simple rgb value like 100, 100, 100
+          // example use "--joy-shadowRing": "0 0 0 30px rgba(var(--joy-shadowChannel, 255 255 255) / 0.1)",
           "--joy-shadowChannel": theme.vars.palette[colors[index]].darkChannel,
+          // mui uses emotion libary for styling
+          // https://www.w3schools.com/cssref/css_selectors.php
+          // https://www.w3schools.com/css/css_pseudo_classes.asp
+          // https://emotion.sh/docs/nested
           [`& .${tabClasses.root}`]: {
             py: 1,
             flex: 1,
@@ -40,17 +65,30 @@ function App() {
           },
         })}
       >
+        {/* 
+          TabList component for displaying individual tabs. 
+          - `variant="soft"` sets a soft styling variant for the TabList.
+        */}
         <TabList
           variant="soft"
           size="md"
           disableUnderline
           sx={{ borderRadius: "lg", p: 0 }}
         >
+          {/* 
+            Individual Tab components for each section. 
+            - `orientation="vertical"` sets the tabs to display vertically.
+            - Conditional styling based on the selected tab index using `... (index === 0 && { color: colors[0] })`.
+          */}
           <Tab
             disableIndicator
             orientation="vertical"
-            {... (index === 0 && { color: colors[0] })}
+            {...(index === 0 && { color: colors[0] })}
           >
+            {/* 
+              ListItemDecorator component wraps the icon for consistent styling.
+              Icon and label for the first tab.
+            */}
             <ListItemDecorator>
               <SchoolIcon />
             </ListItemDecorator>
@@ -69,6 +107,7 @@ function App() {
           <Tab
             disableIndicator
             orientation="vertical"
+            /* ... operator used to spread the object properties, here color=colors[2] */
             {...(index === 2 && { color: colors[2] })}
           >
             <ListItemDecorator>
@@ -88,22 +127,32 @@ function App() {
           </Tab>
         </TabList>
       </Tabs>
+
+      {/* 
+        TabContext component for managing tab panels. 
+        - `value={String(index)}` sets the active tab panel based on the current index.
+      */}
       <TabContext value={String(index)}>
+        {/* 
+          TabPanel components for each tab's content. 
+          - The content can be customized for each tab.
+        */}
         <TabPanel value="0">
-          <b>Second</b> tab panel
+          <b>First</b> tab panel content
         </TabPanel>
         <TabPanel value="1">
-          <b>Third</b> tab panel
+          <b>Second</b> tab panel content
         </TabPanel>
         <TabPanel value="2">
-          <b>Third</b> tab panel
+          <b>Third</b> tab panel content
         </TabPanel>
         <TabPanel value="3">
-          <b>Third</b> tab panel
+          <b>Fourth</b> tab panel content
         </TabPanel>
       </TabContext>
     </>
   );
 }
 
+// Export the App component as the default export
 export default App;
