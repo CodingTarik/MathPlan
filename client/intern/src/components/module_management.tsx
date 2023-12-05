4e221e524ddce79cc820b38362940295a76ce935
Isabelle Sauer <isabelle.sauer@noreply.github.com>
2023-12-05 20:15:35 +0100

    adding function for text fields

--- client/intern/src/components/module_management.tsx
+++ client/intern/src/components/module_management.tsx
@@ -1,9 +1,18 @@
-//import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
+function handleButtonClick(values: string[]) {
+  //console.log(addModuleParameters);
+  // if successful reset state
+  
+  console.log(values);
+}
+
 export default function AddModuleFields() {
+  const [addModuleParameters, setAddModuleParameters] = React.useState(Array(5).fill(null));
+
   return (
     <>
     <Box
@@ -20,12 +29,22 @@ export default function AddModuleFields() {
           id="Modulnummer"
           label="Modulnummer"
           defaultValue=""
+          onChange={(event) => {
+            const nextModule = addModuleParameters.slice();
+            nextModule[0] = event.target.value;
+            setAddModuleParameters(nextModule);
+          }}
         />
         <TextField
           required
           id="Modulname"
           label="Modulname"
           defaultValue=""
+          onChange={(event) => {
+            const nextModule = addModuleParameters.slice();
+            nextModule[1] = event.target.value;
+            setAddModuleParameters(nextModule);
+          }}
         />
         <TextField
           required
@@ -35,22 +54,37 @@ export default function AddModuleFields() {
           InputLabelProps={{
             shrink: true,
           }}
+          onChange={(event) => {
+            const nextModule = addModuleParameters.slice();
+            nextModule[2] = event.target.value;
+            setAddModuleParameters(nextModule);
+          }}
         />
         <TextField
           required
           id="Verwendbarkeit"
           label="Verwendbarkeit"
           defaultValue=""
+          onChange={(event) => {
+            const nextModule = addModuleParameters.slice();
+            nextModule[3] = event.target.value;
+            setAddModuleParameters(nextModule);
+          }}
         />
          <TextField
           required
           id="Sprache"
           label="Sprache"
           defaultValue=""
+          onChange={(event) => {
+            const nextModule = addModuleParameters.slice();
+            nextModule[4] = event.target.value;
+            setAddModuleParameters(nextModule);
+          }}
         />
       </div>
     </Box>
-    <Button variant="outlined">Speichern</Button>
+    <Button variant="outlined" onClick = {() => handleButtonClick(addModuleParameters)}>Speichern</Button>
     </>
   );
 }
