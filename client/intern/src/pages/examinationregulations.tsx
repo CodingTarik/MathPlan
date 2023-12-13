956a979e1557f90ca599b2b13758e23c4f63b232
Tarik Azzouzi <32739295+CodingTarik@users.noreply.github.com>
2023-12-13 13:51:38 +0100

    added basic json editor

--- client/intern/src/pages/examinationregulations.tsx
+++ client/intern/src/pages/examinationregulations.tsx
@@ -1,7 +1,9 @@
-import { useRef, useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 //@ts-expect-error json-editor is not typed
 import * as jsonmod from '@json-editor/json-editor';
 import schema from '../assets/examinationregulation.schema.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
 
 // Die Komponente "ExaminationRegulationApp"
 function ExaminationRegulationApp() {
@@ -9,28 +11,38 @@ function ExaminationRegulationApp() {
   const editorRef = useRef<HTMLDivElement | null>(null);
   // Effekt-Hook für die Initialisierung des JSON-Editors
   useEffect(() => {
-      console.log('i fire once');
-      // Prüfe, ob das Ref ein gültiges DOM-Element enthält
-      if (editorRef.current) {
-        // Initialisiere den JSON-Editor mit dem DOM-Element und dem Schema
-        const editor: JSONEditor<HTMLDivElement> = new jsonmod.JSONEditor(
-          editorRef.current,
-          {
-            theme: 'bootstrap5',
-            iconlib: "fontawesome4",
-            schema: schema
-          }
-        );
-        editor
+    // Prüfe, ob das Ref ein gültiges DOM-Element enthält
+    //jsonmod.JSONEditor.plugins.select2.width = '300px';
+    //jsonmod.JSONEditor.plugins.select2.enable = true;
+    if (editorRef.current) {
+      // Initialisiere den JSON-Editor mit dem DOM-Element und dem Schema
+      const editor: JSONEditor<HTMLDivElement> = new jsonmod.JSONEditor(
+        editorRef.current,
+        {
+          theme: 'bootstrap5',
+          iconlib: 'fontawesome4',
+          schema: schema
+        }
+      );
     }
   }, []); // Das leere Abhängigkeitsarray stellt sicher, dass der Effekt nur einmal ausgeführt wird
 
   // Die Komponente rendert ein div-Element und weist ihm das Ref zu
-  return <div ref={editorRef} />;
+  return (
+    <>
+      <div ref={editorRef} />
+    </>
+  );
 }
 
 // Hier wird die Komponente ohne Striktmodus gerendert
 const AppWithoutStrictMode = () => <ExaminationRegulationApp />;
+
+declare global {
+  interface Window {
+    JSONEditor: any;
+  }
+}
 
 // Exportiere die AppWithoutStrictMode-Komponente für die Verwendung in anderen Dateien
 export default AppWithoutStrictMode;
