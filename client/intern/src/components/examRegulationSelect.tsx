41cd8bf08d989a898ffef4e2e62bd71d99c5a912
Tarik Azzouzi <32739295+CodingTarik@users.noreply.github.com>
2024-01-10 12:25:41 +0100

    better view of control with accordion view

--- client/intern/src/components/examRegulationSelect.tsx
+++ client/intern/src/components/examRegulationSelect.tsx
@@ -1,7 +1,18 @@
 import { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
+import SearchIcon from '@mui/icons-material/Search';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+
 import {
+  Accordion,
+  Box,
   Button,
+  AccordionSummary,
+  AccordionDetails,
+  InputAdornment,
   Table,
   TableBody,
   TableCell,
@@ -10,7 +21,9 @@ import {
   TableRow,
   Paper,
   Snackbar,
-  IconButton
+  IconButton,
+  TablePagination,
+  TextField
 } from '@mui/material';
 import { Refresh } from '@mui/icons-material';
 import { CustomSnackbarContent } from './customSnackbarContent';
@@ -27,6 +40,9 @@ const ExamRegulationSelect = () => {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [saveSuccess, setSaveSuccess] = useState(false);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const fetchExamRegulations = async () => {
     try {
@@ -71,50 +87,116 @@ const ExamRegulationSelect = () => {
     setSnackbarOpen(false);
   };
 
+  const handleChangePage = (event: unknown, newPage: number) => {
+    event;
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredExamRegulations = examRegulations.filter((examRegulation) =>
+    examRegulation.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                Internal Name
-                <IconButton color="primary" onClick={handleRefreshList}>
-                  <Refresh />
-                </IconButton>
-              </TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {examRegulations.map((examRegulation, index) => (
-              <TableRow
-                key={index}
-                selected={selectedExamRegulation === examRegulation}
-              >
-                <TableCell>{examRegulation.name}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    onClick={() => handleLoadExamRegulation(examRegulation)}
-                  >
-                    Load
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    color="secondary"
-                    onClick={() => handleDeleteExamRegulation(index)}
-                  >
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
+      <Accordion defaultExpanded sx={{ marginBottom: '20px' }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Control Description</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    Internal Name
+                    <IconButton color="primary" onClick={handleRefreshList}>
+                      <Refresh />
+                    </IconButton>
+                  </TableCell>
+                  <TableCell>
+                    <Box display="flex" justifyContent="space-between">
+                      <Typography>Actions</Typography>
+                      <TextField
+                        label="Search"
+                        variant="outlined"
+                        onChange={handleSearchChange}
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <SearchIcon />
+                            </InputAdornment>
+                          ),
+                          sx: {
+                            borderRadius: '15px',
+                            height: '45px'
+                          }
+                        }}
+                      />
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {filteredExamRegulations
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((examRegulation, index) => (
+                    <TableRow
+                      key={index}
+                      selected={selectedExamRegulation === examRegulation}
+                    >
+                      <TableCell>{examRegulation.name}</TableCell>
+                      <TableCell>
+                        <Button
+                          startIcon={<CloudDownloadIcon />}
+                          variant="outlined"
+                          color="primary"
+                          onClick={() =>
+                            handleLoadExamRegulation(examRegulation)
+                          }
+                          sx={{ marginRight: '10px' }} // Fügt einen rechten Rand hinzu
+                        >
+                          Apply
+                        </Button>
+                        <Button
+                          startIcon={<DeleteIcon />}
+                          variant="outlined"
+                          color="secondary"
+                          onClick={() => handleDeleteExamRegulation(index)}
+                        >
+                          Delete
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={filteredExamRegulations.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableContainer>
+        </AccordionDetails>
+      </Accordion>
       <Snackbar
         anchorOrigin={{
           vertical: 'bottom',
