ce6c496db715641db6c58fb23fa4dfe078466e93
Henig, Tobias <tobias.henig@noreply.github.com>
2024-02-08 20:48:25 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- tests/database.test.js
+++ tests/database.test.js
@@ -76,7 +76,7 @@ beforeAll(async () => {
   // wait 3 sec
   await new Promise(resolve => setTimeout(resolve, 3000));
 });
-// realeasing open connections
+// releasing open connections
 afterAll(() => {
   if (configFile.database.DB_DIALECT !== 'sqlite') {
     db.sequelize.close();
