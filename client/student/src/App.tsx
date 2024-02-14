a8631215155c6ebc556cdbf5b64c0968aa0a0001
Julie Weiss <julie.weiss@noreply.github.com>
2024-02-14 20:45:19 +0100

    added link to website where examination schedule will be entered

--- client/student/src/App.tsx
+++ client/student/src/App.tsx
@@ -1,18 +1,7 @@
 // Importing necessary dependencies and styles
 import './App.css';
-/* import ListItemDecorator from '@mui/joy/ListItemDecorator';
-import Tabs from '@mui/joy/Tabs';
-import TabList from '@mui/joy/TabList';
-import Tab, { tabClasses } from '@mui/joy/Tab'; 
-import React from 'react';
-import TabPanel from '@mui/lab/TabPanel';
-import { TabContext } from '@mui/lab';
-import SchoolIcon from '@mui/icons-material/School';
-import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-import PolicyIcon from '@mui/icons-material/Policy';
-import SettingsIcon from '@mui/icons-material/Settings';
+import { HashRouter, Route, Routes, Link} from 'react-router-dom';
 
-import AddModuleFields from './components/module_management'; */
 /**
  * JSX structure representing the entire intern application.
  *
@@ -20,17 +9,27 @@ import AddModuleFields from './components/module_management'; */
  * @returns {JSX.Element} The rendered React element.
  */
 function App() {
-  // State to manage the currently selected tab index
-  /* const [index, setIndex] = React.useState(0);
-
-  // Color options for different tabs
-  const colors = ['primary', 'neutral', 'success', 'warning'] as const; */
-
+  const Layout = () => {
+    return (
+      <nav>
+          <Link to="/about">Home</Link>
+    </nav>
+    )
+  }
+  const Home = () => {
+    return (
+      <h1>Hallo</h1>
+    )
+  }
   // JSX structure representing the entire application
   return (
-    <>
-      Hinweis
-    </>
+      <HashRouter>
+      <div><Routes>
+        <Route path="/" element={<Layout />}/>
+        <Route path="/about" element={<Home />}/>
+        </Routes>
+      </div>
+      </HashRouter>
   );
 }
 
