3bb1ad4302916781fe64546e7f1681cf26d88ddf
MatthiasBDr <104760599+MatthiasBDr@users.noreply.github.com>
2024-02-14 21:15:19 +0100

    minor changes

--- client/intern/src/components/uploadPdfToServer.tsx
+++ client/intern/src/components/uploadPdfToServer.tsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import React from 'react';
 import axios from 'axios';
-
+import ModuleServices from '../database_services/ModuleServices';
 /**
  * Styled component for the input element of the file upload button
  * @param props 
@@ -61,14 +61,73 @@ export default function pdfFileUpload() {
       />
     </Button> 
     </div>
-  
   );
-
-  }
+}
   
-  function uploadPdfToDatabase(data:Array<Array<JSON>>|null){
-    // not yet implemented
-    console.log('Nun sollten die Module in die Datenbank geschrieben werden: ');
-    console.dir(data);
-  }
+async function uploadPdfToDatabase(data:Array<Array<JSON>>|null){
+  if(data == null) return;
+  let numAddedModules =0;
+  let numChangedModules =0;
+  let numUnChangedModules =0;
+  let numErrorModules =0;
+  const listOfNotFullyFilledModules = [];
+  /**
+   * Compares the Module from the database with the inputted Module from the pdf, 
+   * if they are the same, the number of unchanged modules is increased, 
+   * if the inputted module is different from the one in the database, the database module is updated, if the module is fully filled
+   * @param module the module from the database
+   * @param values the inputted module from the pdf
+   */
+  const compareModules = (module: { moduleID: string; moduleName: string; moduleCredits: number; moduleLanguage: string; moduleApplicability: string; createdAt: object; id: object, updatedAt: object}, values: string[]) => {
+    if (module.moduleID === values[0] && module.moduleName === values[1] && module.moduleCredits === parseInt(values[2]) && module.moduleLanguage === values[3] && module.moduleApplicability === values[4]) {
+      numUnChangedModules++;
+    } else if(parseInt(values[5]) != 0){
+      ModuleServices.update(values[0], {
+        moduleID: values[0],
+        moduleName: values[1],
+        moduleCredits: values[2],
+        moduleLanguage: values[3],
+        moduleApplicability: values[4]
+      }).then(()=> numChangedModules++).catch(()=> numErrorModules++);//
+    } else {
+      numErrorModules++;
+    }
+  } 
+  
+  await Promise.all(data.map( async (element) => { //loop through the array of arrays
+    await Promise.all(element.map(async (tmp) => { //loop through the array of objects, each object represents a module
+      const values: string[] = Object.values(tmp); //convert the object to an array
+      values[2] = values[2].slice(0, -3);
+      if(parseInt(values[5]) != 0){ // check if the module is fully filled
+        listOfNotFullyFilledModules.push(values);  
+      }
+      console.log('try getbyID'); 
+      try{
+        ModuleServices.getByID(values[0]).then((response:{ data: { moduleID: string; moduleName: string; moduleCredits: number; moduleLanguage: string; moduleApplicability: string; createdAt: object; id: object, updatedAt: object}; })=>{
+          if(response.data){
+            console.log('öffne compareModules');
+            compareModules(response.data, values);
+          }
+          else{//sollte aufgerufen werden, wenn getByID fehlschlägt
+            console.log('öffne create');
+            ModuleServices.create(values.slice(0,5)).then(()=> 
+              {numAddedModules++
+              console.log('numAdded increased')}).catch(()=> 
+                numErrorModules++);
+          }
+        });
+        
+      }catch (e) { //sollte aufgerufen werden, wenn getByID fehlschlägt
+        console.log('im getid catch block gelandet');
+        //ModuleServices.create(values).then(()=> 
+          //numAddedModules++).catch(()=> 
+           // numErrorModules++);
+      }
+    }));
+  }));
+
+  alert("Anzahl hinzugefügter Module: " + numAddedModules + "\nAnzahl geänderter Module: " + numChangedModules + "\nAnzahl unveränderter Module: " + numUnChangedModules + "\nAnzahl nicht hinzugefügter Module: " + numErrorModules);
+  
+}
+  
   
