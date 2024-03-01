5a7b0b41011e781040fad3b18e1eabdace606627
Tarik Azzouzi <32739295+CodingTarik@users.noreply.github.com>
2024-03-01 16:44:57 +0100

    update jest timeout

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+    testTimeout: 30000 // Timeout of 30000 milliseconds (30 seconds) for all tests
+  };
+  
+  module.exports = config;
