59a6dd933bc8b1320b3e3324caefb70457a3917e
Isabelle Sauer <isabelle.sauer@noreply.github.com>
2024-03-10 01:33:49 +0100

    Fix: examplan über student statt intern routen

--- routes/api/student.js
+++ routes/api/student.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
+// for exam plans
+const dbController = require(
+  path.join(__dirname, '../../controllers/databaseController.js')
+);
+
+// for exam plans
+router.post('/addExamPlan', dbController.addExamPlan);
 
 module.exports = router;
